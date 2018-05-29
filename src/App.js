077c120584322e521fa1c44c267aabdafe2679ad
Gabriel Zarate <gabrielzzarate@gmail.com>
2018-05-29 08:34:57 -0400

    refactor into functional presentational components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,68 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import logo from './logo.svg';
+import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER } from './actions/types';
 import './App.css';
+import { store } from './index';
+import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo';
+import Footer from './components/Footer';
+
+let nextTodoId = 0;
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	getVisibleTodos(todos, filter) {
+		switch (filter) {
+			case 'SHOW_ALL':
+				return todos;
+			case 'SHOW_COMPLETED':
+				return todos.filter(t => t.completed);
+			case 'SHOW_ACTIVE':
+				return todos.filter(t => !t.completed);
+			default:
+				return todos;
+		}
+	}
+	render() {
+		const { todos, visibilityFilter } = this.props.state;
+		return (
+			<div className="App">
+				<AddTodo
+					onAddClick={text =>
+						store.dispatch({
+							type: ADD_TODO,
+							id: nextTodoId++,
+							text
+						})
+					}
+				/>
+				<TodoList
+					todos={this.getVisibleTodos(todos, visibilityFilter)}
+					onTodoClick={id =>
+						store.dispatch({
+							type: TOGGLE_TODO,
+							id
+						})
+					}
+				/>
+				<Footer
+					visibilityFilter={visibilityFilter}
+					onFilterClick={filter =>
+						store.dispatch({
+							type: SET_VISIBILITY_FILTER,
+							filter
+						})
+					}
+				/>
+			</div>
+		);
+	}
 }
 
-export default App;
+const mapStateToProps = ({ ...state }) => {
+	return {
+		state
+	};
+};
+
+export default connect(mapStateToProps, null)(App);
